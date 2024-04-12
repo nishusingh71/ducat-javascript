8d163feaca2e424e5972a79a01d5afd9051ff2e1
Nishu Singh <53341807+nishusingh71@users.noreply.github.com>
2024-04-12 16:09:14 +0000

    up

--- AdvancedDataType.js
+++ AdvancedDataType.js
@@ -19,8 +19,73 @@ console.log(Math.trunc(n)); //10
 console.log(n.toFixed(1)); // 10.3
 
 //Imprecise Calc
-console.log(10+20+true-false-false+true-100); // -68
+console.log(10 + 20 + true - false - false + true - 100); // -68
 
 // Tests: isFinite and isNaN
-//Math.round
-console.log(Math.floor(Math.random()));
+//isFinite()
+let a = {};
+console.log(isFinite(a));
+//Number
+a = 12;
+console.log(isFinite(a));
+a = Infinity;
+console.log(isFinite(a));
+a = NaN;
+console.log(isFinite(a));
+//String
+a = "";
+console.log(isFinite(a));
+a = "hw";
+console.log(isFinite(a));
+//Boolean true-> 0,1
+//Null true-> 0
+//Undefined false
+//array
+// a=[][number]-true;
+// false[String]array length greater than 1
+//Object-false
+//Math.random
+//NaN
+//number
+//true NaN
+//false Infinity countable value
+//String
+//true String has somevalue
+// false blank String
+//boolean false
+//Null false
+//Undefined true
+//Array 
+//true[String]array.length greater than 1
+//false[][number]
+//Object true
+
+//Math.random()
+console.log(Math.floor(Math.random() * 100));
+//Math.max()
+console.log(Math.max(10, 20, 30, 40, 50));
+//Math.min()
+console.log(Math.min(10, 20, 30, 40, 50));
+//ParseInt & ParseFloat
+n = "10.2525225";
+console.log(parseInt(n));
+console.log(parseFloat(n));
+
+//Strings
+let str1 = "DUCAT";
+let str2 = 'DUCAT';
+let str3 = `DUCAT`;
+let firstName = 'Nishu';
+let lastName = 'Singh';
+let fullName = `${firstName} ${lastName} ${Math.round()}`;
+console.log(fullName);
+
+//Special Characters
+str1 = "DuCAT\'s";
+str2 = "DUCAT\'s";
+str3 = `DUCAT\'s`;
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+//String length
